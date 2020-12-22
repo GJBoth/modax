daaf2cce376d8be78fa7dab849ca668c9a8f3188
Gert-Jan <gert-jan.both@cri-paris.org>
2020-12-22 21:50:14 +0000

    Added multitask deepmod model.

--- src/modax/models.py
+++ src/modax/models.py
@@ -1,7 +1,7 @@
 from typing import Sequence
-from modax.feature_generators import library_backward
-from modax.layers import LeastSquares
-from modax.networks import MLP
+from modax.feature_generators import library_backward, library_forward
+from modax.layers import LeastSquares, LeastSquaresMT
+from modax.networks import MLP, MultiTaskMLP
 from flax import linen as nn
 
 
@@ -17,3 +17,20 @@ class Deepmod(nn.Module):
         coeffs = LeastSquares()((theta, dt))
         return prediction, dt, theta, coeffs
 
+
+class DeepmodMultiExp(nn.Module):
+    """Simple feed-forward NN.
+    """
+
+    shared_features: Sequence[int]  # this is dataclass, so we dont use __init__
+    specific_features: Sequence[int]
+    n_tasks: int
+
+    @nn.compact  # this function decorator lazily intializes the model, so it makes the layers the first time we call it
+    def __call__(self, inputs):
+        prediction, dt, theta = library_forward(
+            MultiTaskMLP(self.shared_features, self.specific_features, self.n_tasks),
+            inputs,
+        )
+        coeffs = LeastSquaresMT()((theta, dt))
+        return prediction, dt, theta, coeffs
