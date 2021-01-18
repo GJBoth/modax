1910fe35b875935417a90bdee3e73ac0a0f8ff5b
Gert-Jan <gert-jan.both@cri-paris.org>
2021-01-18 14:59:25 +0000

    Checked prior calculations.

--- src/modax/losses/utils.py
+++ src/modax/losses/utils.py
@@ -8,7 +8,7 @@ def mse(y_pred, y):
     """
 
     def squared_error(y, y_pred):
-        return jnp.inner(y - y_pred, y - y_pred) / 2.0
+        return jnp.inner(y - y_pred, y - y_pred)
 
     return jnp.mean(jax.vmap(squared_error)(y, y_pred), axis=0)
 
