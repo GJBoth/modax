11fb6e5eb2cffb71e7f19799690e4fbebc672ae2
Gert-Jan <gert-jan.both@cri-paris.org>
2021-02-01 08:10:45 +0000

    Added metrics to forward solver.

--- src/modax/utils/forward_solver.py
+++ src/modax/utils/forward_solver.py
@@ -7,41 +7,46 @@ from functools import partial
 @partial(jit, static_argnums=(0,))
 def fwd_solver(f, z_init, tol=1e-4, max_iter=300):
     def cond_fun(carry):
-        iteration, z_prev, z = carry
-        # we check the change in alpha (element 0 in z tuple)
-        # and the maximum number of iterations
-        cond_norm = jnp.linalg.norm(z_prev[:-1] - z[:-1]) < tol
+        _, _, (iteration, gap) = carry
+        cond_norm = gap < tol
         cond_iter = iteration >= max_iter
         return ~jnp.logical_or(cond_norm, cond_iter)
 
     def body_fun(carry):
-        iteration, _, z = carry
-        return iteration + 1, z, f(z)
+        z, z_prev, (iteration, gap) = carry
+        gap = jnp.linalg.norm(z[:-1] - z_prev[:-1])
+        return f(z), z, (iteration + 1, gap)
 
-    init_carry = (0, z_init, f(z_init))  # first arg is iteration count
-    _, _, z_star = lax.while_loop(cond_fun, body_fun, init_carry)
-    return z_star
+    init_carry = (
+        f(z_init),
+        z_init,
+        (0, 10 * tol),
+    )
+    z_star, _, metrics = lax.while_loop(cond_fun, body_fun, init_carry)
+    return z_star, metrics
 
 
 # Custom backprop function for iterative methods
 @partial(jax.custom_vjp, nondiff_argnums=(0,))
 @partial(jit, static_argnums=(0,))
-def fixed_point_solver(f, args, z_init, tol=1e-5, max_iter=300):
-    z_star = fwd_solver(
+def fixed_point_solver(f, args, z_init, tol=1e-4, max_iter=300):
+    z_star, metrics = fwd_solver(
         lambda z: f(z, *args), z_init=z_init, tol=tol, max_iter=max_iter
     )
-    return z_star
+    return z_star, metrics
 
 
 @partial(jit, static_argnums=(0,))
 def fixed_point_solver_fwd(f, args, z_init, tol, max_iter):
-    z_star = fixed_point_solver(f, args, z_init, tol, max_iter)
-    return z_star, (z_star, tol, max_iter, args)
+    z_star, metrics = fixed_point_solver(f, args, z_init, tol, max_iter)
+    return (z_star, metrics), (z_star, tol, max_iter, args)
 
 
 @partial(jit, static_argnums=(0,))
 def fixed_point_solver_bwd(f, res, z_star_bar):
     z_star, tol, max_iter, args = res
+    z_star_bar = z_star_bar[0]  # we dont take the gradient w.r.t metric
+
     _, vjp_a = jax.vjp(lambda args: f(z_star, *args), args)
     _, vjp_z = jax.vjp(lambda z: f(z, *args), z_star)
     res = vjp_a(
@@ -50,7 +55,7 @@ def fixed_point_solver_bwd(f, res, z_star_bar):
             z_init=jnp.zeros_like(z_star),
             tol=tol,
             max_iter=max_iter,
-        )
+        )[0]
     )
     return (*res, None, None, None)  # None for init and to
 
