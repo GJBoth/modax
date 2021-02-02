fcdead281f5c3edebb9a2a2d2646724bd35fb06d
Gert-Jan <gert-jan.both@cri-paris.org>
2021-02-02 08:53:49 +0000

    Added Bayesian regression to loss funcs.

--- tests/train_bayesian_regression.py
+++ tests/train_bayesian_regression.py
@@ -1,14 +1,13 @@
 # %% Imports
-from jax import random, numpy as jnp
+from jax import numpy as jnp, random
 
 from modax.data.burgers import burgers
-from modax.losses.standard import loss_fn_pinn_stateful
 from modax.models import Deepmod
-from modax.training import train
-from modax.linear_model.mask_estimator import ThresholdedLasso
-
+from modax.training.utils import create_update
 from flax import optim
 
+from modax.training.losses.bayesian_regression import loss_fn_bayesian_ridge
+from modax.training import train_max_iter
 
 # %% Making data
 key = random.PRNGKey(42)
@@ -22,8 +21,7 @@ X = jnp.concatenate([t_grid.reshape(-1, 1), x_grid.reshape(-1, 1)], axis=1)
 y = u.reshape(-1, 1)
 y += 0.10 * jnp.std(y) * random.normal(key, y.shape)
 
-# %%
-
+# %% Building model and params
 model = Deepmod([30, 30, 30, 1])
 variables = model.init(key, X)
 
@@ -31,18 +29,7 @@ optimizer = optim.Adam(learning_rate=2e-3, beta1=0.99, beta2=0.99)
 state, params = variables.pop("params")
 optimizer = optimizer.create(params)
 
-mask_fn = ThresholdedLasso(threshold=0.1)
+state = (state, {"prior_init": None})  # adding prior to state
+update_fn = create_update(loss_fn_bayesian_ridge, (model, X, y, True))
 
-optimizer, state = train(
-    model,
-    optimizer,
-    state,
-    loss_fn_pinn_stateful,
-    mask_fn,
-    X,
-    y,
-    max_epochs=1e4,
-    split=0.8,
-    rand_seed=42,
-)
-
+optimizer, state = train_max_iter(update_fn, optimizer, state, 10000)
