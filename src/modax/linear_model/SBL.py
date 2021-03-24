f1cbfa6bec0260d0bd6e548ed54b123a18bea273
Gert-Jan <gert-jan.both@cri-paris.org>
2021-03-24 10:11:58 +0000

    New SBL function works.

--- src/modax/linear_model/SBL.py
+++ src/modax/linear_model/SBL.py
@@ -1,89 +1,111 @@
 import jax
 from jax import jit, numpy as jnp
 from modax.utils.forward_solver import fixed_point_solver
+from jax.scipy.linalg import solve_triangular
+from jax.numpy.linalg import cholesky
+from jax.lax import stop_gradient
 
 
+def update_posterior(gram, XT_y, prior):
+    alpha, beta = prior
+    L = cholesky(jnp.diag(alpha) + beta * gram)
+    R = solve_triangular(L, jnp.eye(alpha.shape[0]), check_finite=False, lower=True)
+    sigma = jnp.dot(R.T, R)
+    mean = beta * jnp.dot(sigma, XT_y)
+
+    return mean, sigma
+
+
+def update_prior(X, y, posterior, prior, hyper_prior):
+    n_samples, n_features = X.shape
+    mean, covariance = posterior
+    alpha, beta = prior
+    alpha_prior, beta_prior = hyper_prior
+
+    rmse = jnp.sum((y - jnp.dot(X, mean)) ** 2)
+    gamma = 1.0 - alpha * jnp.diag(covariance)
+
+    # Update alpha and beta
+    alpha = (gamma + 2.0 * alpha_prior[0]) / (
+        (mean.squeeze() ** 2 + 2.0 * alpha_prior[1])
+    )
+    beta = (n_samples - jnp.sum(gamma) + 2.0 * beta_prior[0]) / (
+        rmse + 2.0 * beta_prior[1]
+    )
+
+    # Calculating dL/da
+    dLda = (
+        1
+        / alpha
+        * (
+            1 / 2 * (1 - alpha * (mean.squeeze() ** 2 + jnp.diag(covariance)))
+            + alpha_prior[0]
+            - alpha * alpha_prior[1]
+        )
+    )
+    return jnp.minimum(1e7, alpha), jnp.minimum(1e7, beta), jnp.mean(jnp.abs(dLda))
+
+
+def update(prior, X, y, gram, XT_y, hyper_prior):
+    alpha, beta = prior[:-2], prior[-2]
+    posterior = update_posterior(gram, XT_y, (alpha, beta))
+    alpha, beta, loss_grad = update_prior(X, y, posterior, (alpha, beta), hyper_prior)
+
+    return jnp.concatenate([alpha, beta[jnp.newaxis], loss_grad[jnp.newaxis]], axis=0)
+
+
+def evidence(X, y, gram, XT_y, prior, hyper_prior):
+    n_samples, n_features = X.shape
+    alpha, beta = prior[:-1], prior[-1]
+    alpha_prior, beta_prior = hyper_prior
+
+    mean, covariance = update_posterior(gram, XT_y, (alpha, beta))
+    rmse = jnp.sum((y - jnp.dot(X, mean)) ** 2)
+
+    score = jnp.sum(alpha_prior[0] * jnp.log(alpha) - alpha_prior[1] * alpha)
+    score += beta_prior[0] * jnp.log(beta) - beta_prior[1] * beta
+    score += 0.5 * (
+        jnp.linalg.slogdet(covariance)[1]
+        + n_samples * jnp.log(beta)
+        + jnp.sum(jnp.log(alpha))
+    )
+    score -= 0.5 * (beta * rmse + jnp.sum(alpha * mean.squeeze() ** 2))
+
+    return score.squeeze(), mean
+
+
+@jit
 def SBL(
     X,
     y,
     prior_init=None,
-    alpha_prior=(1e-6, 1e-6),
-    beta_prior=(1e-6, 1e-6),
-    tol=1e-3,
-    max_iter=300,
+    hyper_prior=((1e-6, 1e-6), (1e-6, 1e-6)),
+    tol=1e-5,
+    max_iter=1000,
+    stop_prior_grad=True,
 ):
+
     n_samples, n_features = X.shape
-    norm_weight = jnp.concatenate((jnp.ones((n_features,)), jnp.zeros((1,))), axis=0)
     if prior_init is None:
-        prior_init = jnp.concatenate(
-            [jnp.ones((n_features,)), (1.0 / (jnp.var(y) + 1e-7))[jnp.newaxis]], axis=0
-        )
-    # adding zeros to z for coeffs
+        prior_init = jnp.ones((n_features + 2,))
+        prior_init = jax.ops.index_update(
+            prior_init, n_features, 1 / (jnp.var(y) + 1e-6)
+        )  # setting initial noise value
+
     gram = jnp.dot(X.T, X)
     XT_y = jnp.dot(X.T, y)
 
     prior_params, metrics = fixed_point_solver(
         update,
-        (X, y, gram, XT_y, alpha_prior, beta_prior),
+        (X, y, gram, XT_y, hyper_prior),
         prior_init,
-        norm_weight,
-        tol=tol,
+        lambda z_prev, z: z[-1] > tol,
         max_iter=max_iter,
     )
 
-    loss, mn = evidence(X, y, prior_params, gram, XT_y, alpha_prior, beta_prior)
-
-    return loss, mn, prior_params, metrics
-
-
-def update_sigma(gram, alpha, beta):
-    sigma_inv = jnp.diag(alpha) + beta * gram
-    L_inv = jnp.linalg.pinv(jnp.linalg.cholesky(sigma_inv))
-    sigma_ = jnp.dot(L_inv.T, L_inv)
-    return sigma_
-
-
-def update_coeff(XT_y, beta, sigma_):
-    coef_ = beta * jnp.linalg.multi_dot([sigma_, XT_y])
-    return coef_
-
-
-def update(prior, X, y, gram, XT_y, alpha_prior, beta_prior):
-    n_samples, n_features = X.shape
-    alpha, beta = prior[:-1], prior[-1]
-    sigma = update_sigma(gram, alpha, beta)
-    coeffs = update_coeff(XT_y, beta, sigma)
-
-    # Update alpha and lambda
-    rmse_ = jnp.sum((y - jnp.dot(X, coeffs)) ** 2)
-    gamma_ = 1.0 - alpha * jnp.diag(sigma)
-
-    # TODO: Cap alpha with some threshold.
-    alpha = (gamma_ + 2.0 * alpha_prior[0]) / (
-        (coeffs.squeeze() ** 2 + 2.0 * alpha_prior[1])
-    )
-    beta = (n_samples - gamma_.sum() + 2.0 * beta_prior[0]) / (
-        rmse_ + 2.0 * beta_prior[1]
+    prior = jax.lax.cond(
+        stop_prior_grad, lambda x: stop_gradient(x[:-1]), lambda x: x[:-1], prior_params
     )
 
-    return jnp.concatenate([alpha, beta[jnp.newaxis]], axis=0)
-
-
-def evidence(X, y, prior, gram, XT_y, alpha_prior, beta_prior):
-    n_samples, n_features = X.shape
-    alpha, beta = prior[:-1], prior[-1]
-
-    sigma = update_sigma(gram, alpha, beta)
-    coeffs = update_coeff(XT_y, beta, sigma)
-    rmse_ = jnp.sum((y - jnp.dot(X, coeffs)) ** 2)
-
-    score = jnp.sum(alpha_prior[0] * jnp.log(alpha) - alpha_prior[1] * alpha)
-    score += beta_prior[0] * jnp.log(beta) - beta_prior[1] * beta
-    score += 0.5 * (
-        jnp.linalg.slogdet(sigma)[1]
-        + n_samples * jnp.log(beta)
-        + jnp.sum(jnp.log(alpha))
-    )
-    score -= 0.5 * (beta * rmse_ + jnp.sum(alpha * coeffs.squeeze() ** 2))
-
-    return score.squeeze(), coeffs
+    loss, mn = evidence(X, y, gram, XT_y, prior, hyper_prior)
+    return loss, mn, prior, metrics
--- src/modax/utils/forward_solver.py
+++ src/modax/utils/forward_solver.py
@@ -4,82 +4,43 @@ from jax import jit, numpy as jnp, lax
 from functools import partial
 
 
-def fwd_solver_simple(f, z_init, norm_weight=None, tol=1e-4, max_iter=300):
-    # n_features calculates the norm over the first n_features of z.
-    # Useful for when you're iterating over a but check your convergence on b
-    # such as with SBL.
-
-    def while_loop(cond_fun, body_fun, init_val):
-        val = init_val
-        while cond_fun(val):
-            val = body_fun(val)
-        return val
-
-    @jit
-    def cond_fun(carry):
-        _, _, (iteration, gap) = carry
-        cond_norm = gap < tol
-        cond_iter = iteration >= max_iter
-        return ~jnp.logical_or(cond_norm, cond_iter)
-
-    @jit
-    def body_fun(carry):
-        z, z_prev, (iteration, gap) = carry
-        gap = jnp.linalg.norm((z - z_prev) * norm_weight)
-        return f(z), z, (iteration + 1, gap)
-
-    init_carry = (
-        f(z_init),
-        z_init,
-        (0, 10 * tol),
-    )
-    if norm_weight is None:
-        norm_weight = jnp.ones_like(z_init)
-    z_star, _, metrics = while_loop(cond_fun, body_fun, init_carry)
-    return z_star, metrics
-
-
-@partial(jit, static_argnums=(0,))
-def fwd_solver(f, z_init, norm_weight=None, tol=1e-4, max_iter=300):
-    # n_features calculates the norm over the first n_features of z.
-    # Useful for when you're iterating over a but check your convergence on b
-    # such as with SBL.
-    def cond_fun(carry):
-        _, _, (iteration, gap) = carry
-        cond_norm = gap < tol
-        cond_iter = iteration >= max_iter
-        return ~jnp.logical_or(cond_norm, cond_iter)
+@partial(jit, static_argnums=(0, 2))
+def fwd_solver(f, z_init, cond_fun, max_iter=300):
+    def _cond_fun(carry):
+        z_prev, z, iteration = carry
+        return jax.lax.cond(
+            iteration >= max_iter,
+            lambda _: False,
+            lambda args: cond_fun(*args),
+            (z_prev, z),
+        )
 
     def body_fun(carry):
-        z, z_prev, (iteration, gap) = carry
-        gap = jnp.linalg.norm((z - z_prev) * norm_weight)
-        return f(z), z, (iteration + 1, gap)
+        _, z, iteration = carry
+        return z, f(z), iteration + 1
 
     init_carry = (
-        f(z_init),
         z_init,
-        (0, 10 * tol),
+        f(z_init),
+        0,
     )
-    if norm_weight is None:
-        norm_weight = jnp.ones_like(z_init)
-
-    z_star, _, metrics = lax.while_loop(cond_fun, body_fun, init_carry)
+    z_star, _, metrics = lax.while_loop(_cond_fun, body_fun, init_carry)
     return z_star, metrics
 
 
 # Custom backprop function for iterative methods
-@partial(jax.custom_vjp, nondiff_argnums=(0,))
-@partial(jit, static_argnums=(0,))
-def fixed_point_solver(f, args, z_init, norm_weight=None, tol=1e-4, max_iter=300):
+@partial(jax.custom_vjp, nondiff_argnums=(0, 3))
+@partial(jit, static_argnums=(0, 3))
+def fixed_point_solver(f, args, z_init, cond_fun, max_iter=300):
     z_star, metrics = fwd_solver(
-        lambda z: f(z, *args), z_init, norm_weight, tol=tol, max_iter=max_iter,
+        lambda z: f(z, *args), z_init, cond_fun, max_iter=max_iter,
     )
     return z_star, metrics
 
 
-@partial(jit, static_argnums=(0,))
-def fixed_point_solver_fwd(f, args, z_init, norm_weight, tol, max_iter):
-    z_star, metrics = fixed_point_solver(f, args, z_init, norm_weight, tol, max_iter)
+@partial(jit, static_argnums=(0, 3))
+def fixed_point_solver_fwd(f, args, z_init, cond_fun, max_iter):
+    z_star, metrics = fixed_point_solver(f, args, z_init, cond_fun, max_iter)
     return (z_star, metrics), (z_star, args)
 
 
@@ -94,11 +55,11 @@ def fixed_point_solver_bwd(f, res, z_star_bar):
         fwd_solver(
             lambda u: vjp_z(u)[0] + z_star_bar,
             jnp.zeros_like(z_star),
-            tol=1e-5,
+            lambda z_prev, z: jnp.linalg(z - z_prev) > 1e-5,
             max_iter=5000,
         )[0]
     )
-    return (*res, None, None, None, None)  # None for init and to
+    return (*res, None, None)  # None for init and max_iter
 
 
 fixed_point_solver.defvjp(fixed_point_solver_fwd, fixed_point_solver_bwd)
