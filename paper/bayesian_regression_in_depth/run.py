0cb99f64904686a4ea64e4e82f732f106afcf160
Gert-Jan <gert-jan.both@cri-paris.org>
2021-03-29 09:22:33 +0000

    Added bayesian regression indepth

---
+++ paper/bayesian_regression_in_depth/run.py
@@ -0,0 +1,79 @@
+# %% Imports
+from jax import numpy as jnp, random
+import jax
+from modax.data.burgers import burgers
+from modax.models import Deepmod
+from modax.training.utils import create_update
+from flax import optim
+
+from modax.training import train_max_iter
+from modax.training.losses.standard import loss_fn_pinn
+from modax.training.losses.bayesian_regression import loss_fn_bayesian_ridge
+
+script_dir = "/home/gert-jan/Documents/modax/paper/bayesian_regression_in_depth/runs/"
+key = random.PRNGKey(42)
+noise = 0.10
+n_runs = 1
+max_iterations = 5000
+
+# Making data
+x = jnp.linspace(-3, 4, 50)
+t = jnp.linspace(0.5, 5.0, 20)
+t_grid, x_grid = jnp.meshgrid(t, x, indexing="ij")
+u = burgers(x_grid, t_grid, 0.1, 1.0)
+
+X = jnp.concatenate([t_grid.reshape(-1, 1), x_grid.reshape(-1, 1)], axis=1)
+y = u.reshape(-1, 1)
+y += noise * jnp.std(y) * random.normal(key, y.shape)
+
+
+# Defning model and optimizers
+model = Deepmod([30, 30, 30, 1])
+optimizer_def = optim.Adam(learning_rate=2e-3, beta1=0.99, beta2=0.99)
+
+# Running PINN
+update_fn = create_update(loss_fn_pinn, (model, X, y))
+for run_idx, subkey in enumerate(random.split(key, n_runs)):
+    print(f"Starting multitask run {run_idx}")
+    variables = model.init(subkey, X)
+    state, params = variables.pop("params")
+    optimizer = optimizer_def.create(params)
+    train_max_iter(
+        update_fn,
+        optimizer,
+        state,
+        max_iterations,
+        log_dir=script_dir + f"pinn_run_{run_idx}/",
+    )
+
+# Running warm restart bayes
+update_fn = create_update(loss_fn_bayesian_ridge, (model, X, y, True))
+for run_idx, subkey in enumerate(random.split(key, n_runs)):
+    print(f"Starting bayes warm run {run_idx}")
+    variables = model.init(subkey, X)
+    state, params = variables.pop("params")
+    state = (state, {"prior_init": None})  # adding prior to state
+    optimizer = optimizer_def.create(params)
+    train_max_iter(
+        update_fn,
+        optimizer,
+        state,
+        max_iterations,
+        log_dir=script_dir + f"warm_run_{run_idx}/",
+    )
+
+# Running normal PINN
+update_fn = create_update(loss_fn_bayesian_ridge, (model, X, y, False))
+for run_idx, subkey in enumerate(random.split(key, n_runs)):
+    print(f"Starting pinn cold run {run_idx}")
+    variables = model.init(subkey, X)
+    state, params = variables.pop("params")
+    state = (state, {"prior_init": None})  # adding prior to state
+    optimizer = optimizer_def.create(params)
+    train_max_iter(
+        update_fn,
+        optimizer,
+        state,
+        max_iterations,
+        log_dir=script_dir + f"cold_run_{run_idx}/",
+    )
