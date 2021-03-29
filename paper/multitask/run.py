f00b7312f1d6c4d119c79d66d62754cbd77795d5
Gert-Jan <gert-jan.both@cri-paris.org>
2021-03-29 09:22:21 +0000

    Updated multitask folder exp. domain.

--- paper/multitask/run.py
+++ paper/multitask/run.py
@@ -20,8 +20,8 @@ n_runs = 1
 max_iterations = 5000
 
 # Making data
-x = jnp.linspace(-10, 10, 100)
-t = jnp.linspace(0.1, 1.0, 10)
+x = jnp.linspace(-3, 4, 50)
+t = jnp.linspace(0.1, 5.0, 2)
 t_grid, x_grid = jnp.meshgrid(t, x, indexing="ij")
 u = burgers(x_grid, t_grid, 0.1, 1.0)
 
