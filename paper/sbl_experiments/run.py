3ec62038db33c16a83aad0a62a1615dde4bb3d5d
Gert-Jan <gert-jan.both@cri-paris.org>
2021-03-31 07:55:35 +0000

    Added experiments with data sparsity.

--- paper/sbl_experiments/run.py
+++ paper/sbl_experiments/run.py
@@ -13,6 +13,7 @@ key = random.PRNGKey(42)
 n_runs = 1
 max_iterations = 5000
 
+"""
 # Running noise levels
 x = jnp.linspace(-3, 4, 50)
 t = jnp.linspace(0.5, 5.0, 20)
@@ -41,4 +42,32 @@ for noise in noise_levels:
             state,
             max_iterations,
             log_dir=script_dir + f"noise_{noise:.2f}_run_{run_idx}/",
+        )
+"""
+# RUnning different sparsity.
+# Defning model and optimizers
+model = Deepmod([30, 30, 30, 1])
+optimizer_def = optim.Adam(learning_rate=2e-3, beta1=0.99, beta2=0.99)
+for n_x in jnp.arange(5, 101, 5):
+    x = jnp.linspace(-3, 4, n_x)
+    t = jnp.linspace(0.5, 5.0, 20)
+    t_grid, x_grid = jnp.meshgrid(t, x, indexing="ij")
+    u = burgers(x_grid, t_grid, 0.1, 1.0)
+    X = jnp.concatenate([t_grid.reshape(-1, 1), x_grid.reshape(-1, 1)], axis=1)
+    y = u.reshape(-1, 1)
+    y_noisy = y + 0.10 * jnp.std(y) * random.normal(key, y.shape)
+
+    update_fn = create_update(loss_fn_SBL, (model, X, y_noisy, True))
+    for run_idx, subkey in enumerate(random.split(key, n_runs)):
+        print(f"Starting sparsity run {run_idx}")
+        variables = model.init(subkey, X)
+        state, params = variables.pop("params")
+        state = (state, {"prior_init": None})  # adding prior to state
+        optimizer = optimizer_def.create(params)
+        train_max_iter(
+            update_fn,
+            optimizer,
+            state,
+            max_iterations,
+            log_dir=script_dir + f"nx_{n_x}_run_{run_idx}/",
         )
