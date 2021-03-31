c6cb77e3454009f0d2d3f8390433a3ba1a81d282
Gert-Jan <gert-jan.both@cri-paris.org>
2021-03-31 11:17:16 +0000

    Notebook comparing explicit to implicit diff for bayesian ridge.

---
+++ notebooks/differentiable_fwd_solver/bayesian_regression.py
@@ -0,0 +1,88 @@
+# %% Imports
+from jax import jit, numpy as jnp
+from jax.lax import stop_gradient
+import jax
+from functools import partial
+
+
+@partial(jit, static_argnums=(0,))
+def bayesian_regression(
+    solver,
+    X,
+    y,
+    prior_init=None,
+    hyper_prior=((1e-6, 1e-6), (1e-6, 1e-6)),
+    tol=1e-5,
+    max_iter=300,
+):
+
+    n_samples, n_features = X.shape
+    # Prepping matrices
+    XT_y = jnp.dot(X.T, y)
+    _, S, Vh = jnp.linalg.svd(X, full_matrices=False)
+    eigen_vals = S ** 2
+
+    if prior_init is None:
+        prior_init = jnp.ones((2,))
+        prior_init = jax.ops.index_update(prior_init, 1, 1 / (jnp.var(y) + 1e-7))
+
+    # Running
+    prior_params, iterations = solver(
+        update,
+        (X, y, eigen_vals, Vh, XT_y, hyper_prior),
+        prior_init,
+        lambda z_prev, z: jnp.linalg.norm(z_prev[0] - z[0]) > tol,
+        max_iter=max_iter,
+    )
+
+    log_LL, mn = evidence(X, y, prior_params, eigen_vals, Vh, XT_y, hyper_prior)
+    metrics = (iterations, 0.0)
+    return log_LL, mn, prior_params, metrics
+
+
+@jit
+def update(prior, X, y, eigen_vals, Vh, XT_y, hyper_prior):
+
+    n_samples, n_features = X.shape
+    alpha, beta = prior[:-1], prior[-1]
+    alpha_prior, beta_prior = hyper_prior
+    # Calculating coeffs
+    coeffs = jnp.linalg.multi_dot(
+        [Vh.T, Vh / (eigen_vals + alpha / beta)[:, jnp.newaxis], XT_y]
+    )
+    gamma_ = jnp.sum((beta * eigen_vals) / (alpha + beta * eigen_vals))
+    rmse_ = jnp.sum((y - jnp.dot(X, coeffs)) ** 2)
+
+    # %% Update alpha and lambda according to (MacKay, 1992)
+    alpha = (gamma_ + 2 * alpha_prior[0]) / (jnp.sum(coeffs ** 2) + 2 * alpha_prior[1])
+    beta = (n_samples - gamma_ + 2 * beta_prior[0]) / (rmse_ + 2 * beta_prior[1])
+    return jnp.stack([alpha, beta], axis=0)
+
+
+@jit
+def evidence(X, y, prior, eigen_vals, Vh, XT_y, hyper_prior):
+    # compute the log of the determinant of the posterior covariance.
+    # posterior covariance is given by
+    alpha_prior, beta_prior = hyper_prior
+    alpha, beta = prior[:-1], prior[-1]
+    n_samples, n_features = X.shape
+    coeffs = jnp.linalg.multi_dot(
+        [Vh.T, Vh / (eigen_vals + alpha / beta)[:, jnp.newaxis], XT_y]
+    )
+
+    rmse = jnp.sum((y - jnp.dot(X, coeffs)) ** 2)
+    logdet_sigma = -jnp.sum(jnp.log(alpha + beta * eigen_vals))
+
+    score = alpha_prior[0] * jnp.log(alpha) - alpha_prior[1] * alpha
+    score += beta_prior[0] * jnp.log(beta) - beta_prior[1] * beta
+    score += 0.5 * (
+        n_features * jnp.log(alpha)
+        + n_samples * jnp.log(beta)
+        - beta * rmse
+        - alpha * jnp.sum(coeffs ** 2)
+        + logdet_sigma
+        - n_samples * jnp.log(2 * jnp.pi)
+    )
+
+    return score.squeeze(), coeffs
+
---
+++ notebooks/differentiable_fwd_solver/forward_solver.py
@@ -0,0 +1,103 @@
+# %% Imports
+import jax
+from jax import jit, numpy as jnp, lax
+from functools import partial
+
+# Explicit method
+@partial(jit, static_argnums=(0, 2))
+def fwd_solver_diffable(f, z_init, break_fun, max_iter=300):
+    def while_loop(cond_fun, body_fun, init_val):
+        val = init_val
+        while cond_fun(val):
+            val = body_fun(val)
+        return val
+
+    def _cond_fun(carry):
+        z_prev, z, iteration = carry
+        return jax.lax.cond(
+            iteration >= max_iter,
+            lambda _: False,
+            lambda args: break_fun(*args),
+            (z_prev, z),
+        )
+
+    def body_fun(carry):
+        _, z, iteration = carry
+        return z, f(z), iteration + 1
+
+    init_carry = (
+        z_init,
+        f(z_init),
+        1,
+    )
+    _, z_star, metrics = while_loop(_cond_fun, body_fun, init_carry)
+    return z_star, metrics
+
+
+# Implicit methods
+@partial(jit, static_argnums=(0, 2))
+def fwd_solver(f, z_init, cond_fun, max_iter=300):
+    def _cond_fun(carry):
+        z_prev, z, iteration = carry
+        return jax.lax.cond(
+            iteration >= max_iter,
+            lambda _: False,
+            lambda args: cond_fun(*args),
+            (z_prev, z),
+        )
+
+    def body_fun(carry):
+        _, z, iteration = carry
+        return z, f(z), iteration + 1
+
+    init_carry = (
+        z_init,
+        f(z_init),
+        1,
+    )
+    _, z_star, metrics = lax.while_loop(_cond_fun, body_fun, init_carry)
+    return z_star, metrics
+
+
+# Explicit differentiation function.
+@partial(jit, static_argnums=(0, 3))
+def fixed_point_solver_explicit(f, args, z_init, cond_fun, max_iter=300):
+    z_star, metrics = fwd_solver_diffable(
+        lambda z: f(z, *args), z_init, cond_fun, max_iter=max_iter,
+    )
+    return z_star, metrics
+
+
+# Custom backprop function for iterative methods
+@partial(jax.custom_vjp, nondiff_argnums=(0, 3))
+@partial(jit, static_argnums=(0, 3))
+def fixed_point_solver_implicit(f, args, z_init, cond_fun, max_iter=300):
+    z_star, metrics = fwd_solver(
+        lambda z: f(z, *args), z_init, cond_fun, max_iter=max_iter,
+    )
+    return z_star, metrics
+
+
+def fixed_point_solver_fwd(f, args, z_init, cond_fun, max_iter):
+    z_star, metrics = fixed_point_solver_implicit(f, args, z_init, cond_fun, max_iter)
+    return (z_star, metrics), (z_star, args)
+
+
+def fixed_point_solver_bwd(f, _, res, z_star_bar):
+    z_star, args = res
+    z_star_bar = z_star_bar[0]  # we dont take the gradient w.r.t metric
+
+    _, vjp_a = jax.vjp(lambda args: f(z_star, *args), args)
+    _, vjp_z = jax.vjp(lambda z: f(z, *args), z_star)
+    res = vjp_a(
+        fwd_solver(
+            lambda u: vjp_z(u)[0] + z_star_bar,
+            jnp.zeros_like(z_star),
+            lambda z_prev, z: jnp.linalg.norm(z - z_prev) > 1e-5,
+            max_iter=5000,
+        )[0]
+    )
+    return (*res, None, None)  # None for init and max_iter
+
+
+fixed_point_solver_implicit.defvjp(fixed_point_solver_fwd, fixed_point_solver_bwd)
