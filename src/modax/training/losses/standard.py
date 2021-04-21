bbe774190b31028b6308ab1fdd2063d820d61d0a
Gert-Jan <gert-jan.both@cri-paris.org>
2021-04-21 07:42:36 +0000

    Added theory plots.

--- src/modax/training/losses/standard.py
+++ src/modax/training/losses/standard.py
@@ -10,7 +10,7 @@ def loss_fn_pinn(params, state, model, X, y, l=1.0):
     MSE = mse(prediction, y)
     Reg = mse(dt, theta @ coeffs)
     loss = MSE + l * Reg
-    metrics = {"loss": loss, "mse": MSE, "reg": Reg, "coeff": coeffs}
+    metrics = {"loss": loss, "mse": MSE, "reg": Reg, "coeffs": coeffs}
 
     return loss, (updated_state, metrics, (prediction, dt, theta, coeffs))
 
